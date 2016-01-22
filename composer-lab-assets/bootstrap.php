4f2335658e3c655ab17317bea4d92f1ce7a3b06a
Egbert <49943105@gm.nfu.edu.tw>
2016-01-22 22:14:15 +0800

    first version (change top bar name)

---
+++ composer-lab-assets/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_DATABASE', 'homestead');
+define('DB_CHARSET', 'utf8');
+define('DB_USERNAME', 'homestead');
+define('DB_PASSWORD', 'secret');
