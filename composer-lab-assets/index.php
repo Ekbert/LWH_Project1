a703905b9c1e34433c7111f071cec5915f79a5d0
Egbert <49943105@gm.nfu.edu.tw>
2016-01-22 22:15:50 +0800

    test if manage with git

--- composer-lab-assets/index.php
+++ composer-lab-assets/index.php
@@ -101,7 +101,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=$row->created_at?>
+                    發表於::::: <?=$row->created_at?>
                 </p>
             </div>
             <?php endwhile; ?>
