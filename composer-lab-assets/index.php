e09e4d2ddc660b5f5e59eb05dca9f5be2523b543
Egbert <49943105@gm.nfu.edu.tw>
2016-01-22 22:36:09 +0800

    use carbon

--- composer-lab-assets/index.php
+++ composer-lab-assets/index.php
@@ -2,6 +2,10 @@
 
 require __DIR__.'/bootstrap.php';
 
+\Carbon\Carbon::setLocale('zh');
+use Carbon\Carbon;
+
+
 // connect to dabase
 try {
     $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
@@ -101,7 +105,8 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於::::: <?=$row->created_at?>
+                    <!--發表於::::: <?=$row->created_at?> -->
+                    發表於::::: <?=Carbon::createFromFormat('Y-m-d h:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
             <?php endwhile; ?>
